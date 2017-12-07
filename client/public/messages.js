958a08f02aca27761473191c76873c2f0d6e3232
jazo84 <31776267+jazo84@users.noreply.github.com>
2017-12-07 00:15:30 -0500

    updates

--- client/public/messages.js
+++ client/public/messages.js
@@ -5,12 +5,12 @@ $(document).ready(function(){
   }).then(function(res){
     console.log(res);
     var newRow, messageTitle, messageBody;
-    for(var i = 0; i < res.length; i++) {
-      newRow = $('<tr>');
-      messageTitle = $('<p>');
-      messageBody = $('<p>');
-      messageTitle.text(res[i].title);
-      messageBody.text(res[i].body);
+    for(var i = 0; i < res.messages.length; i++) {
+      newRow = $('<tr class="row">');
+      messageTitle = $('<p id="title">');
+      messageBody = $('<p id="messBody">');
+      messageTitle.text(res.messages[i].title);
+      messageBody.text(res.messages[i].body);
       newRow.append(messageTitle).append(messageBody);
       $('#cardBody').append(newRow);
     }
